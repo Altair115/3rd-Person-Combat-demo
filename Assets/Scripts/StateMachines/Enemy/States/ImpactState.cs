a4905162dda32db63ce0e4903f83a9834a39fac2
K.C <kevin.celinski@outlook.com>
2023-05-19 17:29:27 +0200

    :sparkles: introduced impact State

---
+++ Assets/Scripts/StateMachines/Enemy/States/ImpactState.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace StateMachines.Enemy.States
+{
+    public class ImpactState : EnemyBaseState
+    {
+        private readonly int ImpactHash = Animator.StringToHash("Impact");
+        private const float CrossFadeDuration = 0.1f;
+
+        private float _duration = 1f;
+        
+        public ImpactState(EnemyStateMachine stateMachine) : base(stateMachine)
+        {
+        }
+
+        public override void Enter()
+        {
+            _stateMachine.Animator.CrossFadeInFixedTime(ImpactHash,CrossFadeDuration);
+        }
+
+        public override void Tick(float deltaTime)
+        {
+            Move(deltaTime);
+            
+            _duration -= deltaTime;
+            if (_duration <= 0)
+            {
+                _stateMachine.SwitchState(new IdleState(_stateMachine));
+            }
+        }
+
+        public override void Exit()
+        {
+            
+        }
+    }
+}
--- Assets/Scripts/StateMachines/Player/PlayerBaseState.cs
+++ Assets/Scripts/StateMachines/Player/PlayerBaseState.cs
@@ -1,5 +1,6 @@
 using StateMachines;
 using StateMachines.Player;
+using StateMachines.Player.States;
 using UnityEngine;
 
 public abstract class PlayerBaseState : State
@@ -29,5 +30,17 @@ public abstract class PlayerBaseState : State
         lookPosition.y = 0f;
 
         _stateMachine.transform.rotation = Quaternion.LookRotation(lookPosition);
+    }
+
+    protected void ReturnToLocomotion()
+    {
+        if (_stateMachine.Targeter.CurrentTarget != null)
+        {
+            _stateMachine.SwitchState(new PlayerTargetingState(_stateMachine));
+        }
+        else
+        {
+            _stateMachine.SwitchState(new PlayerFreeLookState(_stateMachine));
+        }
     }
 }
